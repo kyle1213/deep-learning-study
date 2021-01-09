2dac9ad404bc4ad6e5997ae61041e71f0fbb45b3
kyle1213 <jayk01213@naver.com>
2021-01-09 19:52:57 +0900

    loss and accuracy visualization implemented

--- VGG/VGG - cifar-100.py
+++ VGG/VGG - cifar-100.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 
+import matplotlib.pyplot as plt
+
 
 transform = transforms.Compose(
     [transforms.ToTensor(),
@@ -15,7 +17,7 @@ test = torchvision.datasets.CIFAR100(root='CIFAR-100/',
                                      train=False, transform=transform,
                                      download=True)
 
-train_loader = torch.utils.data.DataLoader(dataset=train, batch_size=512, shuffle=True)
+train_loader = torch.utils.data.DataLoader(dataset=train, batch_size=256, shuffle=True)
 train_test_loader = torch.utils.data.DataLoader(dataset=train, batch_size=128, shuffle=True)
 test_loader = torch.utils.data.DataLoader(dataset=test, batch_size=128, shuffle=True)
 
@@ -112,11 +114,19 @@ model = VGG()
 model = model.cuda()
 
 loss = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.00001)
 
 cost = 0
 
-for epoch in range(70):
+iterations = []
+train_losses = []
+test_losses = []
+train_acc = []
+test_acc = []
+
+for epoch in range(100):
+    model.train()
+    correct = 0
     for X, Y in train_loader:
         X = X.to(cuda)
         Y = Y.to(cuda)
@@ -125,8 +135,25 @@ for epoch in range(70):
         cost = loss(hypo, Y)
         cost.backward()
         optimizer.step()
+        prediction = hypo.data.max(1)[1]
+        correct += prediction.eq(Y.data).sum()
+
+    model.eval()
+    correct2 = 0
+    for data, target in test_loader:
+        data = data.to(cuda)
+        target = target.to(cuda)
+        output = model(data)
+        cost2 = loss(output, target)
+        prediction = output.data.max(1)[1]
+        correct2 += prediction.eq(target.data).sum()
 
     print("Epoch : {:>4} / cost : {:>.9}".format(epoch + 1, cost))
+    iterations.append(epoch)
+    train_losses.append(cost.tolist())
+    test_losses.append(cost2.tolist())
+    train_acc.append((100*correct/len(train_loader.dataset)).tolist())
+    test_acc.append((100*correct2/len(test_loader.dataset)).tolist())
 
 
 # del train_loader
@@ -141,25 +168,13 @@ for data, target in test_loader:
     prediction = output.data.max(1)[1]
     correct += prediction.eq(target.data).sum()
 
-print('Test set: Accuracy: {:.2f}%'.format(100. * correct / len(test_loader.dataset))
-      )
-correct = 0
-for data, target in train_test_loader:
-    data = data.to(cuda)
-    target = target.to(cuda)
-    output = model(data)
-    prediction = output.data.max(1)[1]
-    correct += prediction.eq(target.data).sum()
+print('Test set: Accuracy: {:.2f}%'.format(100. * correct / len(test_loader.dataset)))
 
-print('Train set: Accuracy: {:.2f}%'.format(100. * correct / len(train_test_loader.dataset)))
-
-
-
-
-
-
-
-
-
-
-
+plt.subplot(121)
+plt.plot(range(1, len(iterations)+1), train_losses, 'b--')
+plt.plot(range(1, len(iterations)+1), test_losses, 'r--')
+plt.subplot(122)
+plt.plot(range(1, len(iterations)+1), train_acc, 'b-')
+plt.plot(range(1, len(iterations)+1), test_acc, 'r-')
+plt.title('loss and accuracy')
+plt.show()

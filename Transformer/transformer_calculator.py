f25f0e7c963136fbfa6112024d17b9aa1b2d5bfc
kyle1213 <jayk01213@naver.com>
2021-03-21 19:36:06 +0900

    dir changed

--- Transformer/transformer_calculator.py
+++ Transformer/transformer_calculator.py
@@ -1,5 +1,10 @@
 import torch
 import torch.nn as nn
 
+nums = '0 1 2 3 4 5 6 7 8 9'
+n_num = len(nums)
+d_hidn = 128
+nn_emb = nn.Embedding(n_num, d_hidn)
 
-input_embs = nn.Embedding()
+input_embs = nn_emb(inputs) # input embedding
+print(input_embs.size())

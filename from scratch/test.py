231db8d8cf3b0fad49aee1e8374ca2a08e3c2416
kyle1213 <jayk01213@naver.com>
2021-04-04 01:18:01 +0900

    learned torch.nn.Parameters()

--- from scratch/test.py
+++ from scratch/test.py
@@ -1,14 +1,44 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import torch
 
-x = np.arange(0, 6, 0.1)
-y1 = np.sin(x)
-y2 = np.cos(x)
+x = [[1, 2], [3, 4], [5, 6]]
+y = [[1, 4], [9, 16], [25, 36]]
 
-plt.plot(x, y1, label="sin")
-plt.plot(x, y2, label="cos", linestyle="--")
-plt.xlabel("XX")
-plt.ylabel("YY")
-plt.title('sin cos')
-plt.legend()
-plt.show()
+cuda = torch.device('cuda')
+
+class Test(torch.nn.Module):
+    def __init__(self):
+        super(Test, self).__init__()
+        self.plane_weights = torch.nn.Parameter(torch.randn(3, 2).cuda())
+
+    def forward(self, x):
+        x = self.plane_weights * x
+
+        return x
+
+
+model = Test()
+model = model.cuda()
+
+#loss = torch.nn.CrossEntropyLoss()
+loss = torch.nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01, weight_decay=5e-4, momentum=0.9)
+cost = 0
+
+x = torch.tensor(x)
+y = torch.tensor(y)
+
+for epoch in range(150):
+    model.train()
+    torch.cuda.empty_cache()
+    X = x.to(cuda)
+    Y = y.to(cuda)
+    optimizer.zero_grad()
+    output = model(X)
+    cost = loss(output, Y.float())
+    cost.backward()
+    optimizer.step()
+
+for parameter in model.parameters():
+    print(parameter)
